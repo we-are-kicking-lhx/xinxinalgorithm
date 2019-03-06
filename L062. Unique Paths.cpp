25aa8bcbea24dd154ecc7b605e8b43266ea221be
ITACHI哈哈哈 <1030415526@vip.jiangnan.edu.cn>
2019-03-06 17:17:01 +0800

    Update L062. Unique Paths.cpp

--- L062. Unique Paths.cpp
+++ L062. Unique Paths.cpp
@@ -18,7 +18,7 @@ public:
 class Solution {
 public:
 	int uniquePaths(int m, int n) {
-		int dp[100][100];
+		int dp[101][101];
 		for (int i = 1; i <= m; i++)
 			for (int j = 1; j <= n; j++) {
 				if (i == 1 || j == 1)dp[i][j] = 1;
