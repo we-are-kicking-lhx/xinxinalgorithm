1fa18ffc52ec5486cadf85fe74e2679061d142bb
Lliuhaoxin <1917089423@qq.com>
2019-04-06 21:28:58 +0800

    string

---
+++ L1019. Next Greater Node In Linked List.cpp
@@ -0,0 +1,21 @@
+class Solution
+{
+  public:
+    vector<int> nextLargerNodes(ListNode *head)
+    {
+        vector<int> res, stack;
+        for (ListNode *node = head; node; node = node->next)
+        {
+            while (stack.size() && res[stack.back()] < node->val)
+            {
+                res[stack.back()] = node->val;
+                stack.pop_back();
+            }
+            stack.push_back(res.size());
+            res.push_back(node->val);
+        }
+        for (int i : stack)
+            res[i] = 0;
+        return res;
+    }
+};
