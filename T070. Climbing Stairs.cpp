2f039a03f8324f33328e369aff938b8b816da431
plucky <1828151761@qq.com>
2019-04-19 10:53:48 +0800

    dp,溢出修改

--- T070. Climbing Stairs.cpp
+++ T070. Climbing Stairs.cpp
@@ -3,8 +3,7 @@ public:
     int climbStairs(int n) {
         vector<int>dp = vector<int>(n+1);
         dp[1] = 1;
-        dp[2] = 2;
-        
+        if(dp.size() > 2)dp[2] = 2;
         for (int i = 3; i <= n; i++) {
             dp[i] = dp[i - 1] + dp[i - 2];
         }
