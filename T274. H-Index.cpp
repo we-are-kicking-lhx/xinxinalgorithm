e00c3c024f59e9877ec27c3e32c89a27ec72bcc5
plucky <1828151761@qq.com>
2019-07-17 19:34:37 +0800

    排序

---
+++ T274. H-Index.cpp
@@ -0,0 +1,13 @@
+class Solution {
+public:
+    int hIndex(vector<int>& citations) {
+        sort(citations.begin(), citations.end(), greater<int>());
+        for (int i = 0; i < citations.size(); i++) {
+            if(citations[i] == i + 1)
+                return i + 1;
+            if(citations[i] < i + 1)
+                return i;
+        }
+        return citations.size();
+    }
+};
