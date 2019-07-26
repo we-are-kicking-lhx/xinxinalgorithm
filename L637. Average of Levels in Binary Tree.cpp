db6736391959e834e9d3c49f6b6c8a49ae1f3932
Lliuhaoxin <1917089423@qq.com>
2019-07-26 16:10:21 +0800

    Level Order Traversal

---
+++ L637. Average of Levels in Binary Tree.cpp
@@ -0,0 +1,31 @@
+#include<bits/stdc++.h>
+using namespace std;
+
+  struct TreeNode {
+      int val;
+      TreeNode *left;
+      TreeNode *right;
+      TreeNode(int x) : val(x), left(NULL), right(NULL) {}
+  };
+
+class Solution {
+public:
+    vector<double> averageOfLevels(TreeNode* root) {
+        if(!root) return {};
+        vector<double> re;
+        queue<TreeNode*> q{{root}};
+        while(!q.empty()){
+            int n=q.size();
+            double temp=0;
+            for(int i=n;i>0;i--){
+                auto t=q.front();q.pop();
+                temp+=t->val;
+                if(t->left) q.push(t->left);
+                if(t->right) q.push(t->right);
+            }
+            re.push_back(temp/n);
+        }
+        return re;
+    }
+};
+// 参考429
