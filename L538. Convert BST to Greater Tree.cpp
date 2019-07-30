b62da7a539f375692eced042e54d7e19385f40f2
Lliuhaoxin <1917089423@qq.com>
2019-07-30 16:01:46 +0800

    递归

---
+++ L538. Convert BST to Greater Tree.cpp
@@ -0,0 +1,21 @@
+#include<bits/stdc++.h>
+using namespace std;
+struct TreeNode {
+    int val;
+    TreeNode *left;
+    TreeNode *right;
+    TreeNode(int x) : val(x), left(NULL), right(NULL) {}
+};
+
+class Solution {
+public:
+    int sum=0;
+    TreeNode* convertBST(TreeNode* root) {
+        if (!root) return NULL;
+        convertBST(root->right);
+        root->val += sum;
+        sum = root->val;
+        convertBST(root->left);
+        return root;
+    }
+};
