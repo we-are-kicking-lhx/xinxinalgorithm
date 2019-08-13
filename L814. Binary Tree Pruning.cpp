0125e010c1a73e4887ce13fc91c1f9aa7a38ecf5
Lliuhaoxin <1917089423@qq.com>
2019-08-13 21:08:26 +0800

    tree

---
+++ L814. Binary Tree Pruning.cpp
@@ -0,0 +1,19 @@
+#include<bits/stdc++.h>
+struct TreeNode {
+    int val;
+    TreeNode *left;
+    TreeNode *right;
+    TreeNode(int x) : val(x), left(NULL), right(NULL) {}
+};
+ 
+class Solution {
+public:
+    TreeNode* pruneTree(TreeNode* root) {
+        if (!root) return nullptr;
+        root->left = pruneTree(root->left);
+        root->right = pruneTree(root->right);
+        if (!root->left && !root->right)
+            return root->val == 1 ? root : nullptr;
+        return root;
+    }
+};
