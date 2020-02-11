8df5eac1d08eccfb000762cbc21e1f120fdb3e01
plucky <1828151761@qq.com>
2020-02-11 18:44:47 +0800

    二分

---
+++ T162. Find Peak Element.cpp
@@ -0,0 +1,17 @@
+class Solution {
+public:
+    int findPeakElement(vector<int>& nums) {
+        int n = nums.size();
+        int left = 0,right = n - 1;
+        while (left < right) {
+            int mid = (left + right) / 2;
+            if(nums[mid] < nums[mid + 1])
+                left = mid + 1;
+            else
+                right = mid;
+        }
+       return left;
+        // cout << left <<" "<< right << endl;
+        // return 0;
+    }
+};
