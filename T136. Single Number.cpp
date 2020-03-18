ce095eaeca0bb94c433e910d5d08647ac5231e1e
plucky <1828151761@qq.com>
2020-03-18 14:51:22 +0800

    Hash

---
+++ T136. Single Number.cpp
@@ -0,0 +1,10 @@
+class Solution {
+public:
+    int singleNumber(vector<int>& nums) {
+        int res = 0;
+        for(auto &i : nums){
+            res = res ^ i;
+        }
+        return res;
+    }
+};
