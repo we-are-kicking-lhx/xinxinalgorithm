62f0a558cbc7ed6383e25004d075f50ed89ae813
lhx <haoxin18@foxmail.com>
2020-04-04 23:54:32 +0800

    tree

--- L105. Construct Binary Tree from Preorder and Inorder Traversal.cpp
+++ L105. Construct Binary Tree from Preorder and Inorder Traversal.cpp
@@ -14,6 +14,10 @@ public:
     }
     TreeNode* buildTree(vector<int>::iterator preBegin,vector<int>::iterator preEnd,vector<int>::iterator inBegin,vector<int>::iterator inEnd){
         if(preBegin==preEnd) return NULL;
-        
+        TreeNode* cur=new TreeNode(*preBegin);
+        auto root=find(inBegin,inEnd,*preBegin);
+        cur->left=buildTree(preBegin+1,preBegin+1+(root-inBegin),inBegin,root);
+        cur->right=buildTree(preBegin+1+(root-inBegin),preEnd,root+1,inEnd);
+        return cur;
     } 
 };
