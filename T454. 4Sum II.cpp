9851c23fd8b9e9200ec4149cf928cc5e11631164
plucky <1828151761@qq.com>
2020-04-07 11:28:12 +0800

    Hash

---
+++ T454. 4Sum II.cpp
@@ -0,0 +1,17 @@
+class Solution {
+public:
+    int fourSumCount(vector<int>& A, vector<int>& B, vector<int>& C, vector<int>& D) {
+        unordered_map<int,int>hashmap_ab,hashmap_cd;
+        for(auto i : A)
+            for(auto j : B)
+                hashmap_ab[i + j]++;
+        int cnt = 0;
+        for(auto i : C){
+            for(auto j : D){
+                if(hashmap_ab.count(0 - (i + j)) > 0)
+                    cnt += hashmap_ab[0 - (i + j)];
+            }
+        }
+        return cnt;
+    }
+};
