16e90590241ce90d37cc881ed5e8cf03500b7ef0
plucky <1828151761@qq.com>
2020-04-22 17:56:12 +0800

    Tree

--- T144. Binary Tree Preorder Traversal.cpp
+++ T144. Binary Tree Preorder Traversal.cpp
@@ -1,3 +1,4 @@
+//递归
 class Solution {
 public:
     vector<int>res;
@@ -11,6 +12,29 @@ public:
     }
     vector<int> preorderTraversal(TreeNode* root) {
         dfs(root);
+        return res;
+    }
+};
+
+
+//迭代
+class Solution {
+public:
+    vector<int> preorderTraversal(TreeNode* root) {
+        vector<int>res;
+        stack<TreeNode *>s;
+        if(root == NULL)
+            return res;
+        s.push(root);
+        while(!s.empty()){
+            TreeNode *node = s.top();
+            res.push_back(node->val);
+            s.pop();
+            if(node->right != NULL)
+                s.push(node->right);
+            if(node->left != NULL)
+                s.push(node->left);
+        }
         return res;
     }
 };
