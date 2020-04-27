6b50e096c7bdf69ed12a354c48ca9b4351e64f4a
plucky <1828151761@qq.com>
2020-04-27 15:53:35 +0800

    Tree

--- T145. Binary Tree Postorder Traversal.cpp
+++ T145. Binary Tree Postorder Traversal.cpp
@@ -7,6 +7,7 @@
  *     TreeNode(int x) : val(x), left(NULL), right(NULL) {}
  * };
  */
+//递归
 class Solution {
 public:
     vector<int>res;
@@ -21,6 +22,28 @@ public:
     }
     vector<int> postorderTraversal(TreeNode* root) {
         dfs(root);
+        return res;
+    }
+};
+//迭代
+class Solution {
+public:
+    vector<int> postorderTraversal(TreeNode* root) {
+        vector<int>res;
+        if(root == NULL)
+            return res;
+        stack<TreeNode *>s;
+        s.push(root);
+        while(!s.empty()){
+            TreeNode *node = s.top();
+            res.push_back(node->val);
+            s.pop();
+            if(node->left!=NULL)
+                s.push(node->left);
+            if(node->right!=NULL)
+                s.push(node->right);
+        }
+        reverse(res.begin(),res.end());
         return res;
     }
 };
