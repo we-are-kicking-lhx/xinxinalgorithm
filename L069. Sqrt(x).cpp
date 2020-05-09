38b9cd3dbe8150c1416ecd71f83fdda0290a3247
LHX <haoxin18@foxmail.com>
2020-05-09 22:40:22 +0800

    二分

---
+++ L069. Sqrt(x).cpp
@@ -0,0 +1,17 @@
+class Solution {
+public:
+    int mySqrt(int x) {
+        int l = 0, r = x/2+1, ans = -1;
+        while (l <= r) {
+            int mid = l + (r - l) / 2;
+            if ((long long)mid * mid <= x) {
+                ans = mid;
+                l = mid + 1;
+            }
+            else {
+                r = mid - 1;
+            }
+        }
+        return ans;
+    }
+};
