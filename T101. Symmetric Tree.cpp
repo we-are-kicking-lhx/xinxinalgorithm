8ba6ca63fbcce3c26833f5dbb9910486b23f50af
plucky <1828151761@qq.com>
2020-05-11 16:07:53 +0800

    Tree

--- T101. Symmetric Tree.cpp
+++ T101. Symmetric Tree.cpp
@@ -1,44 +1,43 @@
+//递归
+class Solution {
+public:
+    bool sym(TreeNode *n1,TreeNode *n2){
+        if(!(n1||n2))
+            return true;
+        if(!(n1&&n2))
+            return false;
+        return n1->val==n2->val&&sym(n1->left,n2->right)&&sym(n1->right,n2->left);
+    }
+    bool isSymmetric(TreeNode* root) {
+        if(!root)
+            return true;
+        return sym(root->left,root->right);
+    }
+};
+
 //迭代
 class Solution {
 public:
     bool isSymmetric(TreeNode* root) {
+        if(!root)
+            return true;
         queue<TreeNode *>q;
-        q.push(root);
-        q.push(root);
-        while (!q.empty()) {
-            TreeNode * t1 = q.front();
+        q.push(root->left);
+        q.push(root->right);
+        while(!q.empty()){
+            TreeNode *n1 = q.front();
             q.pop();
-            TreeNode * t2 = q.front();
+            TreeNode *n2 = q.front();
             q.pop();
-            if (t1 == NULL && t2 == NULL)
+            if(!(n1||n2))
                 continue;
-            if (t1 == NULL || t2 == NULL)
+            if((!(n1&&n2)) || n1->val!=n2->val)
                 return false;
-            if (t1 -> val != t2 -> val )
-                return false;
-            q.push(t1->left);
-            q.push(t2->right);
-            q.push(t1->right);
-            q.push(t2->left);
+            q.push(n1->left);
+            q.push(n2->right);
+            q.push(n1->right);
+            q.push(n2->left);
         }
-        return true;  
-    }
-};
-
-
-//递归
-class Solution {
-public:
-    bool sym(TreeNode *t1,TreeNode*t2){
-        if (t1 == NULL && t2 == NULL)
-            return true;
-        if (t1 == NULL || t2 == NULL)
-            return false;
-        return (t1->val == t2->val) && sym(t1->left, t2->right) && sym(t1->right, t2->left);
-    }
-    bool isSymmetric(TreeNode* root) {
-        if(root == NULL)
-            return true;
-        return sym(root->left, root->right);
+        return true;
     }
 };
