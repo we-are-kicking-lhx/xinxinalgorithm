51eb2a7136d26219e4f7e1dda515af76f5cfe13a
plucky <1828151761@qq.com>
2020-06-03 17:12:23 +0800

    Tree

--- T116. Populating Next Right Pointers in Each Node.cpp
+++ T116. Populating Next Right Pointers in Each Node.cpp
@@ -1,23 +1,24 @@
 class Solution {
 public:
     Node* connect(Node* root) {
-        if(root == NULL)
-            return root;
+        if(!root)return root;
         queue<Node *>q;
         q.push(root);
-        while (!q.empty()) {
-            int cnt = q.size();
-            Node * next = NULL;
-            while (cnt--) {
-                Node * n = q.front();
+        while(!q.empty()){
+            int level_numbers = q.size();
+            Node *last = NULL;
+            while(level_numbers--){
+                Node *node = q.front();
                 q.pop();
-                n->next = next;
-                if (n->left != NULL) {
-                    q.push(n->right);
-                    q.push(n->left);
-                }
-                next = n;
+                if(last)
+                    last->next = node;
+                last = node;
+                if(node->left)
+                    q.push(node->left);
+                if(node->right)
+                    q.push(node->right);
             }
+            last->next = NULL;
         }
         return root;
     }
