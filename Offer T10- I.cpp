3be40d28fa8a9a4126584e18a146cc37a4763047
PluckySaltyfish <1828151761@qq.com>
2020-11-21 19:41:56 +0800

    DP

---
+++ Offer T10- I.cpp
@@ -0,0 +1,11 @@
+class Solution {
+public:
+    int fib(int n) {
+        int dp[101]={0};
+        dp[1] = 1;
+        for(int i = 2;i <= n; i++){
+            dp[i] = (dp[i-1]+dp[i-2])%1000000007;
+        }
+        return dp[n];
+    }
+};
